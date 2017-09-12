b272f9b0bddefea7170ddd52d236c016ceeb379b
Robert O'Rourke <robert@o-rourke.org>
2017-09-12 12:05:24 +0100

    add defaults and allow overrides from config.yaml

--- modules/chassis-elasticsearch/manifests/init.pp
+++ modules/chassis-elasticsearch/manifests/init.pp
@@ -2,25 +2,45 @@ class chassis-elasticsearch(
   $config
 ) {
 
+  # Default settings for install
+  $defaults = {
+    'version'   => '5.x',
+    'plugins'   => [
+      'analysis-icu'
+    ],
+    'host'      => '0.0.0.0',
+    'port'      => 9200,
+    'timeout'   => 10,
+    'instances' => [
+      'es'
+    ]
+  }
+
+  # Allow override from config.yaml
+  $options = deep_merge( $defaults, $config[elasticsearch] )
+
   # Install Elasticsearch
   class { 'elasticsearch':
     java_install      => true,
     manage_repo       => true,
-    repo_version      => '5.x',
-    restart_on_change => true,
+    repo_version      => $options[version],
+    # Ensure Java doesn't try to eat all the RAMs
+    jvm_options       => [
+      '-Xms512m',
+      '-Xmx512m'
+    ],
     api_protocol      => 'http',
-    api_host          => 'localhost',
-    api_port          => 9200,
-    api_timeout       => 10,
-    validate_tls      => false,
+    api_host          => $options[host],
+    api_port          => $options[port],
+    api_timeout       => $options[timeout],
   }
 
-  # Create an instance
-  elasticsearch::instance { 'es': }
+  # Create instances
+  elasticsearch::instance { $options[instances]: }
 
-  # ICU
-  elasticsearch::plugin { 'analysis-icu':
-    instances => 'es',
+  # Install plugins
+  elasticsearch::plugin { $options[plugins]:
+    instances => $options[instances],
   }
 
 }
