c639b6a0ef9c21891e0d4c4b68b283f552270443
Robert O'Rourke <rob@hmn.md>
2017-09-16 18:30:26 +0100

    It's PHP dumbass

--- local-config.php
+++ local-config.php
@@ -1,2 +1,7 @@
+<?php
+/**
+ * Default elasticsearch host & port.
+ */
+
 defined( 'ELASTICSEARCH_HOST' ) or define( 'ELASTICSEARCH_HOST', '127.0.0.1' );
 defined( 'ELASTICSEARCH_PORT' ) or define( 'ELASTICSEARCH_PORT', 9200 );
