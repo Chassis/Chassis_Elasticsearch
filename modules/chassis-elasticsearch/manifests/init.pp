6d837cacb9dcbea57e9cad3f16da29f479f07ebe
Bronson Quick <bronson@bronsonquick.com.au>
2018-03-09 17:45:11 +1000

    Add disabled_extension support

--- modules/chassis-elasticsearch/manifests/init.pp
+++ modules/chassis-elasticsearch/manifests/init.pp
@@ -2,52 +2,64 @@
 class chassis-elasticsearch(
   $config
 ) {
+  if ( ! empty( $config[disabled_extensions] ) and 'chassis/chassis-elasticsearch' in $config[disabled_extensions] ) {
+    service { 'elasticsearch-es':
+      ensure => stopped,
+      before => Class['elasticsearch']
+    }
+    class { 'elasticsearch':
+      ensure => 'absent'
+    }
+    package { 'java-common':
+      ensure => absent
+    }
+  } else {
 
-  # Default settings for install
-  $defaults = {
-    'repo_version'   => '5.x',
-    'version'        => '5.6.1',
-    'plugins'   => [
-      'analysis-icu'
-    ],
-    'host'      => '0.0.0.0',
-    'port'      => 9200,
-    'timeout'   => 10,
-    'instances' => [
-      'es'
-    ]
-  }
+    # Default settings for install
+    $defaults = {
+      'repo_version' => '5.x',
+      'version'      => '5.6.1',
+      'plugins'      => [
+        'analysis-icu'
+      ],
+      'host'         => '0.0.0.0',
+      'port'         => 9200,
+      'timeout'      => 10,
+      'instances'    => [
+        'es'
+      ]
+    }
 
-  # Allow override from config.yaml
-  $options = deep_merge( $defaults, $config[elasticsearch] )
+    # Allow override from config.yaml
+    $options = deep_merge($defaults, $config[elasticsearch])
 
-  # Install Elasticsearch
-  class { 'elasticsearch':
-    java_install => true,
-    manage_repo  => true,
-    repo_version => $options[repo_version],
-    version      => $options[version],
-    # Ensure Java doesn't try to eat all the RAMs
-    jvm_options  => [
-      '-Xms256m',
-      '-Xmx256m'
-    ],
-    api_protocol => 'http',
-    api_host     => $options[host],
-    api_port     => $options[port],
-    api_timeout  => $options[timeout],
-  }
+    # Install Elasticsearch
+    class { 'elasticsearch':
+      java_install => true,
+      manage_repo  => true,
+      repo_version => $options[repo_version],
+      version      => $options[version],
+      # Ensure Java doesn't try to eat all the RAMs
+      jvm_options  => [
+        '-Xms256m',
+        '-Xmx256m'
+      ],
+      api_protocol => 'http',
+      api_host     => $options[host],
+      api_port     => $options[port],
+      api_timeout  => $options[timeout],
+    }
 
-  # Create instances
-  elasticsearch::instance { $options[instances]:
-    config => {
-      'network.host' => '0.0.0.0'
+    # Create instances
+    elasticsearch::instance { $options[instances]:
+      config => {
+        'network.host' => '0.0.0.0'
+      }
+    }
+
+    # Install plugins
+    elasticsearch::plugin { $options[plugins]:
+      instances => $options[instances],
     }
   }
-
-  # Install plugins
-  elasticsearch::plugin { $options[plugins]:
-    instances => $options[instances],
-  }
-
 }
